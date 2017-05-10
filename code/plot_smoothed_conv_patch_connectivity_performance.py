6b90bad9c1743057d4082cadbce42faa4bd6eaef
djsaunde <dansaunders200713@gmail.com>
2017-05-10 15:45:10 -0400

    fixing up some plotting scripts for ease of use and to make nicer-looking visualizations.

--- code/plot_smoothed_conv_patch_connectivity_performance.py
+++ code/plot_smoothed_conv_patch_connectivity_performance.py
@@ -1,57 +1,61 @@
 from __future__ import division
 
 import matplotlib.pyplot as plt
+import cPickle as p
 import numpy as np
 import os
 
 
 performance_dir = '../performance/conv_patch_connectivity_performance/'
 
+if not os.path.isdir(performance_dir + 'performance_plots_smoothed/'):
+    os.makedirs(performance_dir + 'performance_plots_smoothed/')
+
 def window(size):
     return np.ones(size) / float(size)
 
 print '\n'
-print '\n'.join([ str(idx + 1) + ' | ' + file_name for idx, file_name in enumerate(sorted(os.listdir(performance_dir))) if '.txt' in file_name ])
+print '\n'.join([ str(idx + 1) + ' | ' + file_name for idx, file_name in enumerate(sorted(os.listdir(performance_dir))) if '.p' in file_name ])
 print '\n'
 
 to_plot = raw_input('Enter the index of the file from above which you\'d like to plot: ')
-file_name = sorted([ file_name for file_name in os.listdir(performance_dir) if '.txt' in file_name ])[int(to_plot) - 1]
+file_name = sorted([ file_name for file_name in os.listdir(performance_dir) if '.p' in file_name ])[int(to_plot) - 1]
 
-print file_name
-
-performances = {}
-performance_lines = open(performance_dir + file_name, 'r').readlines()[1:]
-for idx, line in enumerate(performance_lines):
-    voting_mechanism, performance = [ token.strip() for token in line.split(':') ]
-    performances[voting_mechanism] = [ float(token) for token in performance.split() ]
+# get pickled performances dictionary (voting mechanism, performance recordings over training)
+_, performances = p.load(open(performance_dir + file_name, 'rb'))
 
 print '\n'
 
 performance_plots = []
 for voting_mechanism in sorted(performances.keys()):
-    performance_plots.append(plt.plot(np.convolve(performances[voting_mechanism], window(50), 'same'), label=voting_mechanism)[0])
-    # average_plot, = plt.plot([ np.mean(performances[voting_mechanism]) ] * len(performances[voting_mechanism]),
-    #                                             label='average: ' + str(np.mean(performances[voting_mechanism])))
-    # upper_std_plot, = plt.plot([ np.mean(performances[voting_mechanism]) + np.std(performances[voting_mechanism]) ] * 
-    #                                             len(performances[voting_mechanism]), label='plus one standard deviation: ' + 
-    #                                             str(np.mean(performances[voting_mechanism]) + np.std(performances[voting_mechanism])))
-    # lower_std_plot, = plt.plot([ np.mean(performances[voting_mechanism]) - np.std(performances[voting_mechanism]) ] * 
-    #                                             len(performances[voting_mechanism]), label='minus one standard deviation: ' + 
-    #                                             str(np.mean(performances[voting_mechanism]) - np.std(performances[voting_mechanism])))
-    
+    if voting_mechanism in [ 'all', 'most_spiked', 'top_percent', 'spatial_clusters' ]:
+        performance_plots.append(plt.plot(np.convolve(performances[voting_mechanism], window(15), 'same'), label=voting_mechanism)[0])
+
 plt.legend(handles=performance_plots)
 
 fig = plt.gcf()
 fig.set_size_inches(16, 12)
 
-plt.xlabel('Iteration number (1 through ' + str(len(performances[performances.keys()[0]])) + ')')
+plt.xlabel('Iteration number (1 through ' + str(len(performances[performances.keys()[0]]) * 100) + ')')
+plt.xticks([ x for x in xrange(0, len(performances[performances.keys()[0]]) + 25, 25) ], [ x * 100 for x in xrange(0, len(performances[performances.keys()[0]]) + 25, 25) ])
 plt.ylabel('Classification accuracy (out of 100%)')
 
 title_strs = file_name[:file_name.index('weight') - 1].split('_')
 
-# plt.title('Classification accuracy by iteration number (' + title_strs[0] + 'x' + title_strs[0] + ' convolution, stride ' + title_strs[1] + ', ' + title_strs[2] + ' convolution features, giving ' + title_strs[3] + ' excitatory neurons per convolutional patch')
-plt.title(file_name[:file_name.index('.')])
-plt.savefig(performance_dir + 'performance_plots/' + file_name[:file_name.index('.')])
+conv_size = int(file_name.split('_')[1])
+conv_stride = int(file_name.split('_')[2])
+conv_features = int(file_name.split('_')[3])
+lattice_structure = file_name[-7:-6]
+if 'no_weight_sharing' in file_name:
+    weight_sharing = 'no_weight_sharing'
+else:
+    weight_sharing = 'weight_sharing'
+
+plt.title(str(conv_size) + 'x' + str(conv_size) + ' convolutions, stride ' + str(conv_stride) + ', ' + str(conv_features) + \
+                        ' convolution patches, ' + ' '.join(weight_sharing.split('_')) + ', ' + str(lattice_structure) + '-lattice')
+plt.tight_layout()
+
+plt.savefig(performance_dir + 'performance_plots_smoothed/' + file_name[:file_name.index('.')])
 plt.show()
 
 print '\n'
