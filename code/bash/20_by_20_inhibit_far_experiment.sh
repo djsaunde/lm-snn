1fcea286ffc3dd1950b15b1c62f392104cdb7c6e
Dan Saunders <djsaunde@umass.edu>
2017-09-10 15:05:00 -0400

    Updated job submission bash scripts for new command-line arguments.

--- code/bash/20_by_20_inhibit_far_experiment.sh
+++ code/bash/20_by_20_inhibit_far_experiment.sh
@@ -1,16 +1,16 @@
 # Grid search over hyperparameters for experiments with new inhibition schemes.
 
-num_examples=15000
+num_train=15000
 reduced_dataset="True"
 examples_per_class=500
 inhib_scheme="increasing"
 conv_features=400
 
-for random_seed in 1 2 3 4
+for random_seed in 1 2 3
 do
-	for inhib_const in 0.5 1.0 2.0 3.0 4.0 5.0 7.5 10.0
+	for inhib_const in 0.1 0.5 1.0 2.5 5.0
 	do
-		sbatch csnn_pc_inhibit_far_job.sh none 28 0 $conv_features 4 10 $num_examples $reduced_dataset $examples_per_class 8 $inhib_scheme $inhib_const 0.5 False 0.0 $random_seed
+		sbatch csnn_pc_inhibit_far_job.sh none 28 0 $conv_features 4 10 $num_train $reduced_dataset $examples_per_class 8 $inhib_scheme $inhib_const 0.5 False 0.0 $random_seed
 	done
 done
 
