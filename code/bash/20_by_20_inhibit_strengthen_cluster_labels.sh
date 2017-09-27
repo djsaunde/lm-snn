b7e5b9e88f2f0cd9d5de59f96c2b72c465784ee0
Dan Saunders <djsaunde@umass.edu>
2017-09-27 15:47:59 -0400

    Adding bash scripts for running filter clustering experiments.

---
+++ code/bash/20_by_20_inhibit_strengthen_cluster_labels.sh
@@ -0,0 +1,22 @@
+# Grid search over hyperparameters for experiments with new inhibition schemes.
+
+num_train=30000
+reduced_dataset="False"
+examples_per_class=1000
+inhib_scheme="strengthen"
+conv_features=400
+reset_state_vars="False"
+
+for random_seed in 1
+do
+	for inhib_const in 0.5 1.0 2.5 5.0 10.0
+	do
+		for strengthen_const in 0.0 0.05 0.1 0.25 0.5
+		do
+			sbatch csnn_pc_inhibit_far_cluster_labels_job.sh none 28 0 $conv_features 4 10 $num_train $reduced_dataset $examples_per_class \
+							8 $inhib_scheme $inhib_const $strengthen_const False 0.0 $random_seed $reset_state_vars
+		done
+	done
+done
+
+exit
