5ea7038ce9fb0d81ffedb2419aaec1630e1b59f3
Dan Saunders <djsaunde@umass.edu>
2017-10-04 09:10:09 -0400

    Updating bash script.

--- code/bash/20_by_20_growing_inhibition_experiment.sh
+++ code/bash/20_by_20_growing_inhibition_experiment.sh
@@ -1,13 +1,15 @@
 # Grid search over hyperparameters for experiments with new inhibition schemes.
 
-num_train=60000
+num_train=30000
 reduced_dataset="False"
 inhib_scheme="strengthen"
-conv_features=400
 
 for random_seed in 1 2 3
 do
-	sbatch csnn_pc_growing_inhibition_job.sh 28 0 $conv_features $num_train $random_seed
+	for conv_features in 100 225 400 625 900 1225 1600 2025 2500
+	do
+		sbatch csnn_pc_growing_inhibition_job.sh 28 0 $conv_features $num_train $random_seed
+	done
 done
 
 exit
