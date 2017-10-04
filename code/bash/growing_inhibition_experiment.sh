b815352929152d49d8e258cd2939a67877df2845
Dan Saunders <djsaunde@umass.edu>
2017-10-04 09:12:36 -0400

    Fixing name errors in bash script.

--- code/bash/growing_inhibition_experiment.sh
+++ code/bash/growing_inhibition_experiment.sh
@@ -4,11 +4,11 @@ num_train=30000
 reduced_dataset="False"
 inhib_scheme="strengthen"
 
-for random_seed in 1 2 3
+for random_seed in 1 2 3 
 do
 	for conv_features in 100 225 400 625 900 1225 1600 2025 2500
 	do
-		sbatch csnn_pc_growing_inhibition_job.sh 28 0 $conv_features $num_train $random_seed
+		sbatch csnn_growing_inhibition_job.sh 28 0 $conv_features $num_train $random_seed
 	done
 done
 
--- code/bash/growing_inhibition_test.sh
+++ code/bash/growing_inhibition_test.sh
@@ -5,7 +5,7 @@ conv_features=400
 
 for random_seed in 1 2 3
 do
-	sbatch csnn_pc_growing_inhibition_test.sh 28 0 $conv_features $num_train $random_seed
+	sbatch csnn_growing_inhibition_test.sh 28 0 $conv_features $num_train $random_seed
 done
 
 exit
