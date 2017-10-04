2d88cbab516bca24e884701aa90cabf84cd63a40
Dan Saunders <djsaunde@umass.edu>
2017-10-04 12:03:27 -0400

    Updating bash scripts according to adding / max'ing inhibition during the test phase.

--- code/bash/csnn_growing_inhibition_test.sh
+++ code/bash/csnn_growing_inhibition_test.sh
@@ -11,11 +11,13 @@ conv_stride=${2:-0}
 conv_features=${3:-100}
 num_train=${4:-10000}
 random_seed=${5:-42}
+test_no_inhibition=${6:-False}
+test_max_inhibition=${7:-False}
 
 cd ../train/
 
-echo "csnn_growing_inhibition_job.sh"
+echo "csnn_growing_inhibition_test.sh"
 
-python csnn_growing_inhibition.py --mode=test --num_test=10000 --conv_size=$conv_size --conv_stride=$conv_stride --conv_features=$conv_features --num_train=$num_train --random_seed=$random_seed
+python csnn_growing_inhibition.py --mode=test --num_test=10000 --conv_size=$conv_size --conv_stride=$conv_stride --conv_features=$conv_features --num_train=$num_train --random_seed=$random_seed --test_no_inhibition=$test_no_inhibition --test_max_inhibition=$test_max_inhibition
 
 exit
--- code/bash/growing_inhibition_test.sh
+++ code/bash/growing_inhibition_test.sh
@@ -3,9 +3,15 @@
 num_train=30000
 conv_features=400
 
-for random_seed in 1 2 3
+for random_seed in 1 2 3 42
 do
-	sbatch csnn_growing_inhibition_test.sh 28 0 $conv_features $num_train $random_seed
+	for test_no_inhibition in "True" "False"
+	do
+		for test_max_inhibition in "True" "False"
+		do
+			sbatch csnn_growing_inhibition_test.sh 28 0 $conv_features $num_train $random_seed $test_no_inhibition $test_max_inhibition
+		done
+	done
 done
 
 exit
