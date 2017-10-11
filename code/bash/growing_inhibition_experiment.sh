a5ede7d2b3f790e5c2e4069627ee81b0d288c314
Dan Saunders <djsaunde@umass.edu>
2017-10-11 09:42:18 -0400

    Committing local changes.

--- code/bash/growing_inhibition_experiment.sh
+++ code/bash/growing_inhibition_experiment.sh
@@ -1,14 +1,15 @@
 # Grid search over hyperparameters for experiments with new inhibition schemes.
 
-num_train=30000
-
-for random_seed in 1 2 3 4 5
+for num_train in 15000 30000 45000 60000
 do
-	for conv_features in 225 400 625
+	for random_seed in 1 2 3 4 5
 	do
-		for normalized_inputs in True False
+		for conv_features in 225 400 625
 		do
-			sbatch csnn_growing_inhibition_job.sh 28 0 $conv_features $num_train $random_seed $normalized_inputs
+			for normalized_inputs in True False
+			do
+				sbatch csnn_growing_inhibition_job.sh 28 0 $conv_features $num_train $random_seed $normalized_inputs
+			done
 		done
 	done
 done
