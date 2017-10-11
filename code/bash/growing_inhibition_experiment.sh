0e6fce9c8d1da8e966b87d8b4e35235ee3495826
djsaunde <djsaunde@umass.edu>
2017-10-11 09:36:54 -0400

    Fixing errors in the input normalization code.

--- code/bash/growing_inhibition_experiment.sh
+++ code/bash/growing_inhibition_experiment.sh
@@ -6,9 +6,9 @@ for random_seed in 1 2 3 4 5
 do
 	for conv_features in 225 400 625
 	do
-		for normalized_inputs in True False
+		for normalize_inputs in True False
 		do
-			sbatch csnn_growing_inhibition_job.sh 28 0 $conv_features $num_train $random_seed $normalized_inputs
+			sbatch csnn_growing_inhibition_job.sh 28 0 $conv_features $num_train $random_seed $normalize_inputs
 		done
 	done
 done
