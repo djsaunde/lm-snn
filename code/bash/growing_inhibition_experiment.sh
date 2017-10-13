7219d70e745d0a8bf35c626e371b8afb01d01424
Dan Saunders <djsaunde@umass.edu>
2017-10-13 17:25:29 -0400

    Updating bash scripts.sh scripts.

--- code/bash/growing_inhibition_experiment.sh
+++ code/bash/growing_inhibition_experiment.sh
@@ -4,11 +4,14 @@ for num_train in 15000 30000 45000 60000
 do
 	for random_seed in 1 2 3 4 5
 	do
-		for conv_features in 225 400 625
+		for conv_features in 400
 		do
-			for normalized_inputs in True False
+			for normalized_inputs in False
 			do
-				sbatch csnn_growing_inhibition_job.sh 28 0 $conv_features $num_train $random_seed $normalized_inputs
+				for proportion_grow in 0.125 0.25 0.375 0.5 0.625 0.75 0.875 1.0
+				do
+					sbatch csnn_growing_inhibition_job.sh 28 0 $conv_features $num_train $random_seed $normalized_inputs $proportion_grow
+				done
 			done
 		done
 	done
