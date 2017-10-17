4bd965d5da313626d213845740fd7aa9de8a4527
djsaunde <djsaunde@umass.edu>
2017-10-17 13:37:15 -0400

    Adding noise constant to ending of saved files.

--- code/train/csnn_growing_inhibition.py
+++ code/train/csnn_growing_inhibition.py
@@ -1256,7 +1256,8 @@ if __name__ == '__main__':
 
 	# set ending of filename saves
 	ending = '_'.join([ str(conv_size), str(conv_stride), str(conv_features), str(n_e), \
-						str(num_train), str(random_seed), str(normalize_inputs), str(proportion_grow) ])
+						str(num_train), str(random_seed), str(normalize_inputs), 
+						str(proportion_grow), str(noise_const) ])
 
 	b.ion()
 	fig_num = 1
