9a4fbb04d95e076c70e4fcd1c159ea39240a2b91
Dan Saunders <djsaunde@umass.edu>
2017-10-20 13:38:34 -0400

    Adding execution permissions to bash scripts.

---
+++ code/bash/csnn_growing_inhibition_voltage_reset_job.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+#SBATCH --partition=longq
+#SBATCH --time=02-00:00:00
+#SBATCH --mem=30000
+#SBATCH --account=rkozma
+#SBATCH --output=/mnt/nfs/work1/rkozma/djsaunde/stdp-mnist/code/bash/job_reports/%j.out
+
+conv_size=${1:-28}
+conv_stride=${2:-0}
+conv_features=${3:-100}
+num_train=${4:-10000}
+random_seed=${5:-42}
+normalize_inputs=${6:-True}
+proportion_grow=${7:-1.0}
+
+cd ../train/
+
+echo "csnn_growing_inhibition_voltage_reset_job.sh"
+
+python csnn_growing_inhibition.py --mode=train --num_train=$num_train --conv_size=$conv_size --conv_stride=$conv_stride --reset_state_vars=True \
+	--conv_features=$conv_features --random_seed=$random_seed --normalize_inputs=$normalize_inputs --proportion_grow=$proportion_grow
+
+python csnn_growing_inhibition.py --mode=test --num_test=10000 --conv_size=$conv_size --conv_stride=$conv_stride --proportion_grow=$proportion_grow \
+	--conv_features=$conv_features --num_train=$num_train --random_seed=$random_seed --normalize_inputs=$normalize_inputs --reset_state_vars=True
+
+exit
