c1626fb4bb620fd93b1d1b066d8e517a7882035f
djsaunde <djsaunde@umass.edu>
2017-11-02 11:52:22 -0400

    Adding multiple test run script and bash script for re-testing growing inhibition experiments with distance voting scheme.

--- code/bash/growing_inhibition_test_experiment.sh
+++ code/bash/growing_inhibition_test_experiment.sh
@@ -7,11 +7,11 @@ for num_train in 45000 60000
 do
 	for random_seed in 1 2 3
 	do
-		for conv_features in 625
+		for conv_features in 400 625
 		do
 			for proportion_grow in 0.25 0.5 0.75 1.0
 			do
-				sbatch csnn_growing_inhibition_eth_params_job.sh 28 0 $conv_features $num_train $random_seed $normalize_inputs $proportion_grow $noise_const
+				sbatch csnn_growing_inhibition_test_job.sh 28 0 $conv_features $num_train $random_seed $normalize_inputs $proportion_grow $noise_const
 			done
 		done
 	done
