ca1290046cc64efed15fc92ae117dc4565d87dd0
djsaunde <djsaunde@umass.edu>
2017-11-09 17:43:57 -0500

    Adding (hopefully) seizure-safe network training.

--- code/bash/csnn_growing_inhibition_seizure_safe.py
+++ code/bash/csnn_growing_inhibition_seizure_safe.py
@@ -4,7 +4,7 @@
 #SBATCH --time=02-00:00:00
 #SBATCH --mem=30000
 #SBATCH --account=rkozma
-#SBATCH --output=/mnt/nfs/work1/rkozma/djsaunde/stdp-mnist/code/bash/job_reports/%j.out
+#SBATCH --output=/mnt/nfs/work1/rkozma/djsaunde/stdp-mnist/code/bash/seizure_safe_job_reports/%j.out
 
 conv_size=${1:-28}
 conv_stride=${2:-0}
@@ -17,12 +17,12 @@ noise_const=${8:-0.0}
 
 cd ../train/
 
-echo "csnn_growing_inhibition_swap_labeling.sh"
+echo "csnn_growing_inhibition_seizure_safe_job.sh"
 
-python csnn_growing_inhibition_swap_labeling.py --mode=train --num_train=$num_train --conv_size=$conv_size --conv_stride=$conv_stride --noise_const=$noise_const \
+python csnn_growing_inhibition_seizure_safe.py --mode=train --num_train=$num_train --conv_size=$conv_size --conv_stride=$conv_stride --noise_const=$noise_const \
 	--conv_features=$conv_features --random_seed=$random_seed --normalize_inputs=$normalize_inputs --proportion_grow=$proportion_grow
 
-python csnn_growing_inhibition_swap_labeling.py --mode=test --num_test=10000 --conv_size=$conv_size --conv_stride=$conv_stride --proportion_grow=$proportion_grow \
+python csnn_growing_inhibition_seizure_safe.py --mode=test --num_test=10000 --conv_size=$conv_size --conv_stride=$conv_stride --proportion_grow=$proportion_grow \
 	--conv_features=$conv_features --num_train=$num_train --random_seed=$random_seed --normalize_inputs=$normalize_inputs --noise_const=$noise_const
 
 exit
