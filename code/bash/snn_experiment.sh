a496f3c12be82da6b8ca9d0ce03e665b398c7860
Dan Saunders <djsaunde@umass.edu>
2017-11-16 11:59:41 -0500

    Bash script changes.

--- code/bash/snn_experiment.sh
+++ code/bash/snn_experiment.sh
@@ -2,13 +2,13 @@
 
 mkdir snn_job_reports
 
-for num_train in 30000 60000 120000 180000
+for num_train in 60000 120000 180000
 do
 	for random_seed in 1 2 3 4 5 6 7 8 9 10
 	do
-		for conv_features in 400 900 1600 2500 3600 4900 6400
+		for conv_features in 400 900 1600 6400
 		do
-			sbatch snn_mnist_job.sh $conv_features $num_train $random_seed
+			sbatch snn_job.sh $conv_features $num_train $random_seed
 		done
 	done
 done
--- code/bash/snn_job.sh
+++ code/bash/snn_job.sh
@@ -6,16 +6,16 @@
 #SBATCH --account=rkozma
 #SBATCH --output=/mnt/nfs/work1/rkozma/djsaunde/stdp-mnist/code/bash/snn_job_reports/%j.out
 
-conv_features=${3:-100}
-num_train=${4:-10000}
-random_seed=${5:-42}
+conv_features=${1:-100}
+num_train=${2:-10000}
+random_seed=${3:-42}
 
 cd ../train/
 
 echo "snn_job.sh"
 
-python csnn_growing_inhibition.py --mode=train --num_train=$num_train --conv_features=$conv_features --random_seed=$random_seed
+python snn_mnist.py --mode=train --num_train=$num_train --conv_features=$conv_features --random_seed=$random_seed
 
-python csnn_growing_inhibition.py --mode=test --num_train=$num_train --conv_features=$conv_features --random_seed=$random_seed --num_test=10000
+python snn_mnist.py --mode=test --num_train=$num_train --conv_features=$conv_features --random_seed=$random_seed --num_test=10000
 
 exit
