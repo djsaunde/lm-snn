0a0b802e0a18450b77dba30df00de0e3f526c602
djsaunde <djsaunde@umass.edu>
2017-11-16 12:29:24 -0500

    Adding Python and bash scripts for two-levl inhibition experiments.

---
+++ code/bash/two_level_inhibition_experiment.sh
@@ -0,0 +1,22 @@
+# Grid search over hyperparameters for experiments with new inhibition schemes.
+
+for num_train in 60000
+do
+	for random_seed in 1 2 3
+	do
+		for conv_features in 625
+		do
+			for proportion_low in 0.25 0.5 0.75
+			do
+				for start_inhib in 0.1 1.0 2.5
+				do
+					for max_inhib in 10.0 15.0 17.4 20.0
+					do
+						sbatch csnn_two_level_inhibition_job.sh 28 0 $conv_features $num_train \
+											$random_seed $proportion_low $start_inhib $max_inhib
+			done
+		done
+	done
+done
+
+exit
