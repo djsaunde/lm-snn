168e168a9b8bfe5c6822fd0fc4e4196f012fd1bc
djsaunde <djsaunde@umass.edu>
2017-11-28 16:11:18 -0500

    Adding two-level inhibition with restart.

---
+++ code/bash/csnn_two_level_inhibition_retrain_job.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+#SBATCH --partition=longq
+#SBATCH --time=02-00:00:00
+#SBATCH --mem=30000
+#SBATCH --account=rkozma
+#SBATCH --output=/mnt/nfs/work1/rkozma/djsaunde/stdp-mnist/code/bash/job_reports/%j.out
+
+conv_size=${1:-28}
+conv_stride=${2:-0}
+conv_features=${3:-100}
+num_train=${4:-10000}
+random_seed=${5:-42}
+proportion_low=${6:-0.1}
+proportion_high=${7:-0.5}
+start_inhib=${8:-0.1}
+max_inhib=${9:-17.4}
+
+cd ../train/
+
+echo "csnn_two_level_inhibition_retrain_job.sh"
+
+python csnn_two_level_inhibition.py --mode=train --num_train=$num_train --conv_size=$conv_size \
+			--conv_stride=$conv_stride --conv_features=$conv_features --random_seed=$random_seed \
+			--proportion_low=$proportion_low --proportion_high=$proportion_high --start_inhib=$start_inhib --max_inhib=$max_inhib
+
+python csnn_two_level_inhibition.py --mode=test --num_test=10000 --conv_size=$conv_size \
+	--conv_stride=$conv_stride --proportion_low=$proportion_low --proportion_high=$proportion_high --conv_features=$conv_features \
+	--num_train=$num_train --random_seed=$random_seed --start_inhib=$start_inhib --max_inhib=$max_inhib
+
+exit
---
+++ code/bash/two_level_inhibition_retrain_experiment.sh
@@ -0,0 +1,26 @@
+# Grid search over hyperparameters for experiments with new inhibition schemes.
+
+for num_train in 60000
+do
+	for random_seed in 1 2
+	do
+		for conv_features in 625
+		do
+			for proportion_low in 0.1 0.15 0.25 0.5
+			do
+				for proportion_high in 0.5 0.75 1.0
+				do
+					for start_inhib in 0.5 1.0 2.5
+					do
+						for max_inhib in 17.5 20.0
+						do
+							sbatch csnn_two_level_inhibition_job.sh 28 0 $conv_features $num_train \
+								$random_seed $proportion_low $proportion_high $start_inhib $max_inhib
+					done
+				done
+			done
+		done
+	done
+done
+
+exit
